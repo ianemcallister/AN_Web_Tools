0a1cbf56b75e713764dbd5c7195fd04292b1d53a
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2017-08-23 19:15:48 -0700

    NPM Start Working Now

---
+++ server/app.js
@@ -0,0 +1,42 @@
+
+// load all required modules
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//return the express object
+var app = express();
+
+//environment variables
+var port = process.env.PORT || 3000;
+
+//get the URL encoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+//tell it the folder to serve
+app.use(express.static('dist'));
+
+//define our body parsers
+app.use(jsonParser); // for parsing application/json
+app.use(urlencodedParser); // for parsing application/x-www-form-urlencoded
+
+//my own middleware
+app.use('/', function(req, res, next) {
+	//log the url to the console
+	console.log('Request Url: ' + req.url);
+
+	next();
+});
+
+//handle HTTP requests, for GET calls
+app.get('/', function(req, res) {
+
+	//send back html
+	res.send('success');
+
+});
+
+//open the port for local development
+app.listen(port,function() {
+	console.log('Express server is up and running on port ' + port);
+});
